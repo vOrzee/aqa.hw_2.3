5003cb533468f0afdc44b27701eede9b4472ef00
Roman Leshin <vorzee@gmail.com>
2025-01-13 18:36:23 +0500

    Уронили сборку

--- src/test/java/ru/netology/rest/PostmanTests.java
+++ src/test/java/ru/netology/rest/PostmanTests.java
@@ -19,7 +19,7 @@ public class PostmanTests {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("something data"))
         ;
     }
 }
